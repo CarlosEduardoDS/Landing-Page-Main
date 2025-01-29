c1820a863b4a9848fc3e7a766ff075d596ef288e
CARLOS EDUARDO DA SILVA <carlos.silva@pmc.local>
2025-01-29 10:11:33 -0300

    Feito o projeto do frontendMentor

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+document.querySelector("#botao-hamburguer").addEventListener("click", function() {
+    document.querySelector("#menuMobile").style.display = "flex";
+});
+
+document.querySelector("#botaoX").addEventListener("click", function(){
+    document.querySelector("#menuMobile"). style.display = "none";
+});
